6d4a6e3b2ff675f1d8dc004767cc6b8292483598
MagdyMahmoud88 <magdymahmoud8812@gmail.com>
2024-11-23 14:01:06 +0200

    new project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\CreatePost;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/counter',Counter::class)->name('livewire.counter');
+// Route::get('/create',CreatePost::class);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/' , function() {
+    return view('home');
+});
+Route::get('/about' , function() {
+    return view('about');
+});
+Route::get('/contact' , function() {
+    return view('contact');
 });
