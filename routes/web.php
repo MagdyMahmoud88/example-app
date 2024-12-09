b52310eb2d2eeeb4276944086f26ad26ed5091b7
MagdyMahmoud88 <magdymahmoud8812@gmail.com>
2024-12-09 12:56:48 +0200

    update controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Livewire\Counter;
 use App\Livewire\CreatePost;
 use App\Models\Job;
@@ -8,45 +9,35 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-//    lazy loading because more than queries
-//    $jobs = Job::all();
-
-    return view('job.index',
-        [
-            'jobs' => $jobs]
-    );
-});
-
-Route::get('/job/create' , function () {
-    return view('job.create');
-});
-
-Route::get('/job/{id}', function ($id) {
-    $job = Job::find($id);
-// $job = Arr::first($jobs, fn($job)=>$job['id']==$id );
-
-    return view('job.show', ['jobs' => $job]);
-});
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' =>['required' , 'min:3'],
-        'salary' =>['required' ],
-    ]);
-    job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-    return redirect('/jobs');
-});
+Route::view('/' , 'home');
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::resource('jobs', JobController::class);
+
+//Route::get('/', function () {
+//    return view('home');
+//});
+
+
+
+// index
+//Route::get('/jobs', [JobController::class, 'index']);
+//
+// create
+//Route::get('/jobs/create', [JobController::class, 'create']);
+// show
+//Route::get('/job/{job}', [JobController::class, 'show']);
+//
+// store
+//Route::post('/jobs',[JobController::class, 'store']);
+
+//Route::controller(JobController::class)->group(function () {
+//    Route::get('/jobs' , 'index');
+//    Route::get('/jobs/create' , 'create');
+//    Route::post('/jobs' , 'store');
+//    Route::get('/jobs/{job}' , 'show');
+//
+//});
 
